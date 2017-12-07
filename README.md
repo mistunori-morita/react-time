2295a0aa8f180dc4b106f0537ee3cfe3563b0510
mitsunori.morita <mitsunori.morita@speee.jpmitsunori.morita@speee.jpmitsunori.morita@speee.jp>
2017-12-07 19:54:34 +0900

    三項演算子による修正

--- README.md
+++ README.md
@@ -469,3 +469,55 @@ render() {
 }
 
 ```
+
+### ライフサイクルを理解する
+- 無限ループの部分をライフサイクルを利用して修正
+```js
+class Clock extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      days: 0,
+      hours: 0,
+      minutes: 0,
+      seconds: 0
+    }
+  }
+
+  componentWillMount() {
+    this.getTimeUntil(this.props.dedline);
+  }
+
+  componentDidMount() {
+    setInterval(() => this.getTimeUntil(this.props.dedline), 1000)
+  }
+
+  getTimeUntil(dedline) {
+    const time = Date.parse(dedline) - Date.parse(new Date());
+    console.log('time',time);
+
+    const seconds = Math.floor((time/1000)% 60);
+    const minutes = Math.floor((time/1000/60)%60);
+    const hours = Math.floor(time/(1000*60*60) % 24);
+    const days = Math.floor(time/(1000*60*60*24));
+
+    console.log('seconds',seconds,'minutes',minutes,'hours', hours,'days',days);
+    //省略形でsetStateをして更新できるようにする
+    this.setState({
+      days, hours, minutes, seconds
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="Clock-days">{this.state.days} Days</div>
+        <div className="Clock-hours">{this.state.hours}  hours</div>
+        <div className="Clock-minutes">{this.state.minutes}  minutes</div>
+        <div className="Clock-seconds">{this.state.seconds}  seconds</div>
+      </div>
+    )
+  }
+}
+
+```
--- countdownchamp/src/Clock.jsx
+++ countdownchamp/src/Clock.jsx
@@ -13,6 +13,18 @@ class Clock extends Component {
     }
   }
 
+  componentWillMount() {
+    this.getTimeUntil(this.props.dedline);
+  }
+
+  componentDidMount() {
+    setInterval(() => this.getTimeUntil(this.props.dedline), 1000)
+  }
+
+
+  leading0(num) {
+    return num < 10 ? '0' + num : 10;
+  }
 
   getTimeUntil(dedline) {
     const time = Date.parse(dedline) - Date.parse(new Date());
@@ -25,16 +37,18 @@ class Clock extends Component {
 
     console.log('seconds',seconds,'minutes',minutes,'hours', hours,'days',days);
 
+    this.setState({
+      days, hours, minutes, seconds
+    });
   }
 
   render() {
-    this.getTimeUntil(this.props.dedline)
     return (
       <div>
-        <div className="Clock-days">{this.state.days} Days</div>
-        <div className="Clock-hours">{this.state.hours}  hours</div>
-        <div className="Clock-minutes">{this.state.minutes}  minutes</div>
-        <div className="Clock-seconds">{this.state.seconds}  seconds</div>
+        <div className="Clock-days">{this.leading0(this.state.days)} Days</div>
+        <div className="Clock-hours">{this.leading0(this.state.hours)}  hours</div>
+        <div className="Clock-minutes">{this.leading0(this.state.minutes)}  minutes</div>
+        <div className="Clock-seconds">{this.leading0(this.state.seconds)}  seconds</div>
       </div>
     )
   }
